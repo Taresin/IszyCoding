b9d6b0c51e3f346474fffe81fafe3f80fda9d693
SalicBlu3 <dougy_style@hotmail.com>
2018-11-03 15:56:47 +1100

    Fix MnM challenge

--- src/challenges/Challenge02MnMSorting.java
+++ src/challenges/Challenge02MnMSorting.java
@@ -3,13 +3,25 @@ package challenges;
 /**
  * Created by dougylee on 1/11/18.
  *
- * We are picking MnMs out of a jar randomly.
+ * We have lots of MnMs in a jar.
  * There are 3 colors:
  *  • Red
  *  • Green
  *  • Blue
  *
- * Make an interactive game for the user to see which color gets picked the most.
+ * There is a bucket for each of the colors.
+ * 10 MnMs are selected from the jar one at a time.
+ * Each time, we put the MnM in it's respective bucket.
+ *
+ * The bucket with the most MnMs at the end wins.
+ *
+ *
+ * Make a game for the user to see which color gets picked the most.
+ *
+ * 1. How do you represent each color?
+ * 2. How do you represent each bucket?
+ * 3. How would you let the user follow the "story"?
+ *
  */
 public class Challenge02MnMSorting {
 
--- src/challenges/solutions/Challenge02MnMSorting.java
+++ src/challenges/solutions/Challenge02MnMSorting.java
@@ -5,13 +5,25 @@ import java.util.Random;
 /**
  * Created by dougylee on 1/11/18.
  *
- * We are picking MnMs out of a jar randomly.
+ * We have lots of MnMs in a jar.
  * There are 3 colors:
  *  • Red
  *  • Green
  *  • Blue
  *
- * Make an interactive game for the user to see which color gets picked the most.
+ * There is a bucket for each of the colors.
+ * 10 MnMs are selected from the jar one at a time.
+ * Each time, we put the MnM in it's respective bucket.
+ *
+ * The bucket with the most MnMs at the end wins.
+ *
+ *
+ * Make a game for the user to see which color gets picked the most.
+ *
+ * 1. How do you represent each color?
+ * 2. How do you represent each bucket?
+ * 3. How would you let the user follow the "story"?
+ *
  */
 public class Challenge02MnMSorting {
 
@@ -40,6 +52,10 @@ public class Challenge02MnMSorting {
                     System.out.println("Picked a BLUE MnM");
                     break;
             }
+
+            System.out.println("\n----------------\n");
+
+            System.out.printf("Round %d\n", i + 1);
 
             System.out.printf("RED %d\n", redBox);
             System.out.printf("GREEN %d\n", greenBox);
